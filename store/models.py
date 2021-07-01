37a271758004e332111ab55c027fcd12a8b94291
AdityaTaware7 <adityataware0709@gmail.com>
2021-07-01 23:41:15 +0530

    Cart and Checkout rendering done

--- store/models.py
+++ store/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     digital = models.BooleanField(default=False,null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     
+
     def __str__(self):
         return self.name
 
@@ -66,6 +67,7 @@ class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
 
 	@property
 	def get_total(self):
